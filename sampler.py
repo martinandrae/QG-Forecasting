77c799d728c0b2bf307eae6495c0ed44b657a999
mandra <mandra@kth.se>
2024-03-16 13:23:36 +0100

    Fixed again

--- sampler.py
+++ sampler.py
@@ -11,7 +11,7 @@ def edm_sampler(
     sigma_max = min(sigma_max, net.sigma_max)
 
     # Time step discretization.
-    step_indices = torch.arange(num_steps, dtype=torch.float64, device=latents.device)
+    step_indices = torch.arange(num_steps, dtype=torch.float32, device=latents.device)
     t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
     t_steps = torch.cat([net.round_sigma(t_steps), torch.zeros_like(t_steps[:1])]) # t_N = 0
 
@@ -42,47 +42,6 @@ def edm_sampler(
     return x_next #torch.stack(xs) #xs[::-1])
 
 
-def complete_edm_sampler(
-    net, latents, class_labels=None, randn_like=torch.randn_like,
-    num_steps=18, sigma_min=0.002, sigma_max=80, rho=7,
-    S_churn=0, S_min=0, S_max=float('inf'), S_noise=1,
-):
-    # Adjust noise levels based on what's supported by the network.
-    sigma_min = max(sigma_min, net.sigma_min)
-    sigma_max = min(sigma_max, net.sigma_max)
-
-    # Time step discretization.
-    step_indices = torch.arange(num_steps, dtype=torch.float64, device=latents.device)
-    t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
-    t_steps = torch.cat([net.round_sigma(t_steps), torch.zeros_like(t_steps[:1])]) # t_N = 0
-
-    # Main sampling loop.
-    x_next = latents * t_steps[0]
-    xs = [x_next]
-    for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])): # 0, ..., N-1
-        x_cur = x_next
-
-        # Increase noise temporarily.
-        gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
-        t_hat = net.round_sigma(t_cur + gamma * t_cur)
-        x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * randn_like(x_cur)
-
-        # Euler step.
-        denoised = net(x_hat, t_hat, class_labels)#.to(torch.float64)
-        d_cur = (x_hat - denoised) / t_hat
-        x_next = x_hat + (t_next - t_hat) * d_cur
-
-        # Apply 2nd order correction.
-        if i < num_steps - 1:
-            denoised = net(x_next, t_next, class_labels)#.to(torch.float64)
-            d_prime = (x_next - denoised) / t_next
-            x_next = x_hat + (t_next - t_hat) * (0.5 * d_cur + 0.5 * d_prime)
-        
-        xs.append(x_next)
-
-    return x_next, torch.stack(xs) #xs[::-1])
-
-
 def drift_fn(model, x, t, class_labels=None):
     """The drift function of the reverse-time SDE."""
     sigma = t
@@ -97,10 +56,16 @@ def get_div_fn(fn):
   def div_fn(x, t, eps, class_labels=None):
     with torch.enable_grad():
       x.requires_grad_(True)
-      fn_eps = torch.sum(fn(x, t, class_labels) * eps)
-      grad_fn_eps = torch.autograd.grad(fn_eps, x)[0]
-    x.requires_grad_(False)
-    return torch.sum(grad_fn_eps * eps, dim=tuple(range(1, len(x.shape))))
+      fn_eps = torch.sum(fn(x, t, class_labels) * eps, axis=(1,2,3))
+      grad_fn_eps = torch.zeros_like(eps)
+
+      for i in range(eps.size(0)):
+          dl = torch.autograd.grad(fn_eps[i], x, create_graph=True, allow_unused=True)[0]
+          grad_fn_eps[i] = dl
+
+    #x.requires_grad_(False)
+    res = torch.sum(grad_fn_eps * eps, dim=(1,2,3)).mean(dim=0)
+    return res
 
   return div_fn
 
@@ -110,62 +75,64 @@ def div_fn(model, x, t, noise, class_labels=None):
 
 
 def likelihood_sampler(
-    net, latents, samples, class_labels=None, randn_like=torch.randn_like,
+    net, latents, class_labels=None, randn_like=torch.randn_like,
     num_steps=18, sigma_min=0.002, sigma_max=80, rho=7,
     S_churn=0, S_min=0, S_max=float('inf'), S_noise=1,
+    eps_multiplier=1
 ):
     # Adjust noise levels based on what's supported by the network.
     sigma_min = max(sigma_min, net.sigma_min)
     sigma_max = min(sigma_max, net.sigma_max)
 
     # Time step discretization.
-    step_indices = torch.arange(num_steps, dtype=torch.float64, device=latents.device)
+    step_indices = torch.arange(num_steps, dtype=torch.float32, device=latents.device)
     t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
     t_steps = torch.cat([net.round_sigma(t_steps), torch.zeros_like(t_steps[:1])]) # t_N = 0
 
     # Main sampling loop.
-    x_next = samples[0].to(torch.float64)
+    x_next = latents * t_steps[0]
 
-    epsilon = torch.randn_like(x_next)
+    eps_shape = x_next.shape
+    eps_shape = (eps_shape[0] * eps_multiplier,) + eps_shape[1:]  # Double the number of channels
+    epsilon = torch.randn(eps_shape).to(latents.device)
+    
     div_next = div_fn(net, x_next, t_steps[0], epsilon, class_labels)
 
     N = np.prod(latents.shape[1:])
     prior_logp = -N / 2. * np.log(2 * np.pi * sigma_max ** 2) - torch.sum(latents ** 2, dim=(1, 2, 3)) / (2 * sigma_max ** 2)
-
+    prior_logp = prior_logp[0]
     post_logp = prior_logp
 
-    xs = [x_next]
-    logps = [post_logp]
     for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])): # 0, ..., N-1
         x_cur = x_next
 
+        # SAMPLE
         # Increase noise temporarily.
         gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
         t_hat = net.round_sigma(t_cur + gamma * t_cur)
         x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * randn_like(x_cur)
 
         # Euler step.
-        denoised = net(x_hat, t_hat, class_labels).to(torch.float64)
+        denoised = net(x_hat, t_hat, class_labels)#.to(torch.float64)
         d_cur = (x_hat - denoised) / t_hat
         x_next = x_hat + (t_next - t_hat) * d_cur
 
         # Apply 2nd order correction.
         if i < num_steps - 1:
-            denoised = net(x_next, t_next, class_labels).to(torch.float64)
+            denoised = net(x_next, t_next, class_labels)
             d_prime = (x_next - denoised) / t_next
             x_next = x_hat + (t_next - t_hat) * (0.5 * d_cur + 0.5 * d_prime)
         
-        xs.append(x_next)
-
-        # Likelihood
+        # LIKELIHOOD
         div_cur = div_next
         
         if i < num_steps - 1:
-          div_next = div_fn(net, x_next, t_next, epsilon, class_labels)
-          post_logp += (div_cur + div_next) * (t_next - t_cur) / 2
-        else:
-           post_logp += div_cur * (t_next - t_cur) / 2
-        
-        logps.append(post_logp)
+            epsilon = torch.randn(eps_shape).to(latents.device)
+            div_next = div_fn(net, x_next, t_next, epsilon, class_labels)
+            delta_logp = (div_cur + div_next) * (t_next - t_cur) / 2
+            
+            post_logp += delta_logp
 
-    return x_next, torch.stack(xs[::-1]), post_logp, torch.stack(logps[::-1])
+    return x_next, post_logp
+
+
