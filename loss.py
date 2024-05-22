82b1e65f6888db8b389d5d44749628f10e214bc5
mandra <mandra@kth.se>
2024-05-22 09:21:22 +0200

    Added wmse loss and some plots

--- loss.py
+++ loss.py
@@ -35,3 +35,33 @@ class GCLoss:
         return loss
 
 #----------------------------------------------------------------------------
+# Area weighted loss function from the codebase 
+# diffusion-models-for-weather-prediction
+class WeightedMSELoss:
+    def __init__(self, weights):
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.weights = torch.tensor(weights, device=device)   
+
+    def loss_fn(self, input: torch.tensor, target: torch.tensor):
+        return (self.weights * (input - target) ** 2).mean()
+
+class AreaWeightedMSELoss(WeightedMSELoss):
+    def __init__(self, lat, lon):
+        super().__init__(weights=comp_area_weights_simple(lat, lon))
+    
+
+
+def comp_area_weights_simple(lat: np.ndarray, lon: np.ndarray) -> np.ndarray:
+    """An easier way to calculate the (already normalized) area weights.
+
+    Args:
+        lat (np.ndarray): Array of latitudes of grid center points
+        lon (np.ndarray): Array of lontigutes of grid center points
+
+    Returns:
+        np.ndarray: 2d array of relative area sizes.
+    """
+    area_weights = np.cos(lat * (2 * np.pi) / 360)
+    area_weights = area_weights.reshape(-1, 1).repeat(lon.shape[0],axis=-1)
+    area_weights = (lat.shape[0]*lon.shape[0] / np.sum(area_weights)) * area_weights
+    return area_weights
